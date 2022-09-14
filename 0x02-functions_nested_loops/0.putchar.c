0e28a54bf38284ae10c6cf1e961af76c3faf937f
EmPhoenixx <michealodunaike@gmail.com>
2022-09-14 16:17:19 -0700

    functions and nested loops task 0

--- 0x02-functions_nested_loops/0.putchar.c
+++ 0x02-functions_nested_loops/0.putchar.c
@@ -1,21 +1,19 @@
-#include "main.h"
-
-/** * main - Entry point * Return:0 */
-
 /**
- * main - Entry point
- * Return:0
- */
-
-int main(void)
+ * main - putchar.c
+ *
+ * Desceiption:Write a program that prints _putchar, followed by a new line.
+ *
+ * Return: 0
+ */int main(void)
 {
-char c[9] = "_putchar";
-int i = 0;
-while (i < 8)
-{
-_putchar(c[i]);
-i++;
-}
+ __putchar(__);
+_putchar(p);
+_putchar(u);
+   _putchar(t);
+_putchar(c);
+_putchar(h);
+_putchar(a);
+   _putchar(r);
 _putchar(n);
 return (0);
 }
